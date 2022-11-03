4b253ece721c975abaa0a8a132da0dfafb948ce7
Willyan <willy4nnp@gmail.com>
2022-11-03 15:30:17 -0300

    added responsive

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@ const btnMobile = document.getElementById("header-nav-btn-mobile");
 function toggleMenu() {
      const nav = document.getElementById("header-nav");
      nav.classList.toggle('header-nav-active')
-} 
+
+     const menu = document.getElementById("body");
+     menu.classList.toggle('scroll')
+}
 
 btnMobile.addEventListener('click', toggleMenu);
